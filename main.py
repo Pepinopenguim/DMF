1b8d990da983a5c08f2fa74a70da9d21a0983098
Pepinopenguim <vitorlcazevedo@gmail.com>
2025-07-05 17:43:16 -0300

    Added terminal to app

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 import numpy as np
 from tkinter import ttk
+from bird_coords import bird_coords
 
 # This class holds the data for the beam simulation
 class Model():
@@ -44,6 +45,8 @@ class Model():
         
         # Method to set a new length for the beam
         def set_length(self, new_length:float):
+                if new_length == self.length:
+                        return False
                 self.length = new_length
                 # Check if existing supports are still valid with the new length
                 self.check_valid_supports()
@@ -374,7 +377,7 @@ class Pencil():
                         width=self.line_width, 
                         fill=self.eff_fill, 
                         arrow=tk.LAST if magnitude < 0 else tk.FIRST,  # Add an arrowhead to the end of the line
-                        arrowshape=(10, 12, 5) # Customize arrowhead shape (length, fullwidth, halfwidth at base)
+                        arrowshape=(height/6, height/5, height/10) # Customize arrowhead shape (length, fullwidth, halfwidth at base)
                 )
                 # If specified, write the magnitude of the force
                 if write:
@@ -384,10 +387,10 @@ class Pencil():
 
                         # Write force magnitude text on the canvas
                         self.view.maincanvas.create_text(
-                                (start_x, start_y + k * draw_height / 8),
+                                (start_x, start_y + k * draw_height / 5),
                                 text = f"{abs(magnitude):.2f}",
                                 fill = self.eff_fill, 
-                                font = f"TkDefaultFont {int(height / 4)}"
+                                font = f"TkDefaultFont {int(height / 3)}"
                 )
         
         # Draws a distributed load on the beam
@@ -407,11 +410,11 @@ class Pencil():
                 x1 = self.view.canvas_padx + (self.view.canvas_w - 2*self.view.canvas_padx)*(x1 / beam_length)
 
                 # Calculate the number of small arrows to draw for the distributed load
-                N = int(abs(x0 - x1) / (height / 5))
+                N = int(abs(x0 - x1) / (height / 3))
 
                 # Draw a series of small point loads to represent the distributed load
                 for i in np.linspace(x0, x1, N):
-                        print(i)
+                        
                         self.draw_point_load(
                                 beam_position=i,
                                 height=height,
@@ -429,31 +432,40 @@ class Pencil():
                         (x0 + abs(x0 - x1) / 2, self.view.beam_y - draw_height * (7/6)),
                         text = f"{abs(magnitude):.2f}",
                         fill = self.eff_fill, 
-                        font = f"TkDefaultFont {int(height / 4)}"
+                        font = f"TkDefaultFont {int(height / 3)}"
                 )
+        
+        def draw_bird(self, topleft, size, canvas:tk.Canvas, color):
+                
+                bird_coords, eye_coords = bird_coords[:-4], bird_coords[-4:]
+
                 
 
 
-
                 
 
-
-# This class represents the main application window (GUI)
+# main application window (GUI)
 class View(tk.Tk):
         # Initialize the view
         def __init__(self, controller):
                 super().__init__("View")
                 self.controller = controller
-                
+                self.font = ("Helvetica", 10, "bold")
                 # Set window size
                 self.geometry("1000x700")
                 # Create a pencil for drawing
                 self.pencil = Pencil(self)
                 self.pack_propagate = False
-                # Set up the GUI elements
+                # if true, adds gui elements for viewing graphs
+                self.view_solution:bool = False
+
+                # Set terminal variables
+                self.terminal_scroller_offset = 0
+                self.terminal_messages = []
+
                 self.start_gui()
-                # Set up styling for ttk widgets
                 self.setup_styler()
+                
 
         # Configures the style for themed Tkinter widgets
         def setup_styler(self):
@@ -463,14 +475,14 @@ class View(tk.Tk):
                 self.styler.configure("TButton", padding=5) 
 
         # Helper function to create a separator with a title
-        def create_separator(self, frame, text, side = "top"):
+        def create_separator(self, frame, text, side = "top", pady=12):
                 ttk.Separator(frame).pack(fill="x", pady=12, side="top")
-                (ttk.Label(
-                        frame,
-                        text=text,
-                        font=("Arial", 10, "bold")
-                        )
-                ).pack(side=side)  
+                if text is not None:
+                        ttk.Label(
+                                frame,
+                                text=text,
+                                font=self.font
+                        ).pack(side=side)  
         
         # Method to create and arrange all the main GUI components
         def start_gui(self):
@@ -478,13 +490,29 @@ class View(tk.Tk):
                 self.main_frame = ttk.Frame(self)
                 self.main_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
-                # Canvas for drawing the beam
-                self.canvas_frame = ttk.Frame(self.main_frame)
-                self.canvas_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+                # define frame for left widgets
+                self.left_frame = ttk.Frame(self.main_frame)
+                self.left_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+
+                # Configure grid rows for proportional sizing
+                self.left_frame.rowconfigure(0, weight=1)  
+                self.left_frame.rowconfigure(1, weight=1)  
+                self.left_frame.columnconfigure(0, weight=1)
+
+                # Canvas Frame 
+                self.canvas_frame = ttk.Frame(self.left_frame)
+                self.canvas_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=5)
 
                 self.maincanvas = tk.Canvas(self.canvas_frame, bg="white", bd=2, relief="groove")
                 self.maincanvas.pack(fill="both", expand=True, padx=5, pady=5)
 
+                # Terminal Frame
+                self.terminal_frame = ttk.Frame(self.left_frame)
+                self.terminal_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
+
+                self.terminal_canvas = tk.Canvas(self.terminal_frame, bg="#eeeeee", bd=2, relief="groove")
+                self.terminal_canvas.pack(fill="both", expand=True, padx=5)
+                self.terminal_canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
 
                 # Control Panel for user inputs
                 self.control_frame = tk.Frame(self.main_frame, width = 250)
@@ -500,12 +528,28 @@ class View(tk.Tk):
 
         # Creates the GUI elements for setting the beam length
         def length_gui(self):
-                # Length entry field
+                # length section
+                self.create_separator(self.control_frame, text=None, pady=3)
+
+                # frame for sideways packing
+                len_frame = ttk.Frame(self.control_frame)
+                len_frame.pack(pady=3)
+
+                # Length input
+                ttk.Label(
+                        len_frame,
+                        text="Length",
+                        font=self.font
+                ).pack(side="left")
                 self.len_var = tk.StringVar(value="10.0")
-                len_entry = ttk.Entry(self.control_frame, textvariable=self.len_var)
-                len_entry.pack(fill="x", padx=5, pady=2)
+                ttk.Entry(
+                        len_frame,
+                        textvariable=self.len_var
+                ).pack(fill="x", side="left")
+
                 ttk.Button(
-                        self.control_frame, text="Set Length (m)",
+                        self.control_frame,
+                        text="Set Length (m)",
                         command=lambda: self.controller.set_length(self.len_var.get())
                 ).pack(pady=2)
 
@@ -517,7 +561,7 @@ class View(tk.Tk):
                 # Frame for the position input
                 line1 = ttk.Frame(self.control_frame)
                 line1.pack(pady=5)
-                ttk.Label(line1, text="Position", font=("Arial", 10, "bold"), width=10).pack(side="left", padx=2)
+                ttk.Label(line1, text="Position", font=self.font, width=10).pack(side="left", padx=2)
                 self.support_pos_strgvar = tk.StringVar(value="")
                 support_pos_entry = ttk.Entry(line1, textvariable=self.support_pos_strgvar, width = 12)
                 support_pos_entry.pack(side="left")
@@ -593,19 +637,19 @@ class View(tk.Tk):
 
                 # Position input
                 line1.pack(pady=lines_pady)
-                ttk.Label(line1, text="Position", font=("Arial", 10, "bold"), width=10).pack(side="left", padx=2)
+                ttk.Label(line1, text="Position", font=self.font, width=10).pack(side="left", padx=2)
                 self.load_pos_strgvar = tk.StringVar(value="")
                 ttk.Entry(line1, textvariable=self.load_pos_strgvar, width = 12).pack()
                 
                 # Magnitude input
                 line2.pack(pady=lines_pady)
-                ttk.Label(line2, text="Magnitude", font=("Arial", 10, "bold"), width=10).pack(side="left", padx=2)
+                ttk.Label(line2, text="Magnitude", font=self.font, width=10).pack(side="left", padx=2)
                 self.mag_strgvar = tk.StringVar(value="")
                 ttk.Entry(line2, textvariable=self.mag_strgvar, width = 12).pack()
 
                 # Angle input
                 line3.pack(pady=lines_pady)
-                ttk.Label(line3, text="Angle", font=("Arial", 10, "bold"), width=10).pack(side="left", padx=2)
+                ttk.Label(line3, text="Angle", font=self.font, width=10).pack(side="left", padx=2)
                 self.angle_strgvar = tk.StringVar(value="90")
                 ttk.Entry(line3, textvariable=self.angle_strgvar, width = 12).pack()
 
@@ -633,14 +677,87 @@ class View(tk.Tk):
         
         # Creates the GUI element for solving the beam problem
         def solve_gui(self):
-                self.create_separator(frame = self.control_frame, text="")
+                self.create_separator(frame = self.control_frame, text=None, pady=4)
 
                 # Solve button (command not yet implemented)
-                solve_button = ttk.Button(self.control_frame, text="Solve", command=None)
+                solve_button = ttk.Button(self.control_frame, text="Solve", command=self.controller.save_button_clicked)
                 solve_button.pack(pady=2)
 
-        
+                self.after_solve_frame = ttk.Frame(self.control_frame)
+                self.after_solve_frame.pack(pady=3)
 
+        def destroy_after_solve_gui(self):
+                for widget in self.after_solve_frame.winfo_children():
+                        widget.destroy()
+
+        def after_solve_gui(self):
+                self.destroy_after_solve_gui()
+
+                # See Normal 
+                ttk.Button(
+                        self.after_solve_frame,
+                        text="Normal",
+                        command=None
+                ).grid(row=0, column=0, padx=2, pady=2)
+
+                # See Slice 
+                ttk.Button(
+                        self.after_solve_frame,
+                        text="Cortante",
+                        command=None
+                ).grid(row=0, column=1, padx=2, pady=2)
+
+                # See Moment 
+                ttk.Button(
+                        self.after_solve_frame,
+                        text="Moment",
+                        command=None
+                ).grid(row=1, column=0, padx=2, pady=2)
+
+        def on_mouse_wheel(self, event):
+                self.terminal_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+        def add_terminal_message(self, message):
+                self.terminal_messages.append(message)
+
+        def draw_terminal(self):
+                if not self.view_solution:
+                        # clear canvas
+                        self.terminal_canvas.delete("all")
+
+                        # get canvas dimenions
+                        w = self.terminal_canvas.winfo_width()
+                        h = self.terminal_canvas.winfo_height()
+
+                        terminal_font = ("Consolas", 14, "italic")
+
+                        # get font_size
+                        temp_text = self.terminal_canvas.create_text((0,0), text="D", font=terminal_font)
+                        _, y1, _, y2 =  self.terminal_canvas.bbox(temp_text)
+                        font_size = y2 - y1
+                        self.terminal_canvas.delete(temp_text)
+
+                        # define start y coord to write
+                        num_messages = len(self.terminal_messages)
+                        if num_messages * font_size > h:
+                                write_y = - (num_messages - h / font_size) * font_size + h / 50
+                        else:
+                                write_y = font_size                                          
+                        write_x = w / 100
+
+
+                        for message in self.terminal_messages:
+                                self.terminal_canvas.create_text(
+                                        (write_x, write_y),
+                                        fill= "black",
+                                        text= f"> {message}",
+                                        font= terminal_font,
+                                        anchor="w"
+                                )
+
+                                write_y += font_size
+
+                        return
 
         # Redraws the entire canvas
         def draw_beam(self):
@@ -651,10 +768,12 @@ class View(tk.Tk):
                 
 
                 # Define the y-position of the beam on the canvas
-                self.beam_y = self.canvas_h // 3
+                self.beam_y = self.canvas_h // 2
 
                 # Define padding on the sides of the canvas
                 self.canvas_padx = 50
+
+                std_height = self.canvas_h / 10
 
                 # Draw the main beam line
                 self.maincanvas.create_line(self.canvas_padx, self.beam_y, self.canvas_w - self.canvas_padx, self.beam_y, width = 3, fill="black")
@@ -662,15 +781,19 @@ class View(tk.Tk):
                 # Draw all saved supports
                 for support_pos, support_type in self.controller.model.supports:
                         # Call the appropriate drawing function from the pencil's mapper
-                        self.pencil.mapper[support_type](support_pos, self.canvas_h//22, canvas=self.maincanvas)
+                        self.pencil.mapper[support_type](support_pos, std_height, canvas=self.maincanvas)
                         
                 # Draw all saved point forces
                 for magnitude, force_pos, angle in self.controller.model.point_loads:
-                        self.pencil.draw_point_load(beam_position=force_pos, height=self.canvas_h//11, canvas=self.maincanvas, angle=angle, magnitude=magnitude)
+                        self.pencil.draw_point_load(beam_position=force_pos, height=std_height, canvas=self.maincanvas, angle=angle, magnitude=magnitude)
 
                 # Draw all saved distributed loads
                 for pos_limits, magnitude in self.controller.model.loads:
-                        self.pencil.draw_load(pos_limits=pos_limits, height=self.canvas_h//11, canvas=self.maincanvas, magnitude=magnitude)
+                        self.pencil.draw_load(pos_limits=pos_limits, height=std_height, canvas=self.maincanvas, magnitude=magnitude)
+        
+        def update_display(self):
+                self.draw_beam()
+                self.draw_terminal()
 
 # This class acts as the controller (in the MVC pattern), handling user input and updating the model and view
 class Controller():
@@ -685,13 +808,13 @@ class Controller():
         
         # This method is called to refresh the drawing on the canvas
         def update_display(self, event=None):
-
-                self.view.draw_beam()
+                self.view.update_display()
         
         # Handles the "Remove Support" button click
         def remove_last_support(self):
                 # If the model successfully removes a support, update the view
                 if self.model.remove_last_support():
+                        self.view.add_terminal_message("Support Removed.")
                         self.update_display()
                         return True
                 return False
@@ -700,6 +823,7 @@ class Controller():
         def remove_last_effort(self):
                 # If the model successfully removes an effort, update the view
                 if self.model.remove_last_effort():
+                        self.view.add_terminal_message("Effort Removed.")
                         self.update_display()
                         return True
                 return False
@@ -714,6 +838,7 @@ class Controller():
                 
                 # If the model successfully sets the length, update the view
                 if self.model.set_length(new_length):
+                        self.view.add_terminal_message(f"New Length set to:{new_length}")
                         self.update_display()
                         return True
                 return False
@@ -733,19 +858,9 @@ class Controller():
                 
                 # If the model successfully adds the support, update the view
                 if self.model.add_support(position, support_type):
-
-                        # starttemp (Temporary print for debugging)
-                        print("-"*10)
-                        for i in self.model.supports:
-                                print(i)
-                        print("-"*10)
-                        # endtemp
-
+                        self.view.add_terminal_message(f"New support {support_type} added to:{position}")
                         self.update_display()
                         return True
-                
-                
-                
                 
                 return False
 
@@ -776,20 +891,21 @@ class Controller():
                 if pos1 is None:
                         # If the model adds the point load, update the display
                         if self.model.add_point_load(magnitude=magnitude, position=pos0, angle=angle):
+                                self.view.add_terminal_message(f"New force ({magnitude}>{angle}°) added to:{position}")
                                 self.update_display()
-                                print(1)
                                 return True
-                        print(2)
                         return False
 
                 # If pos1 is valid, it's a distributed load
                 if self.model.add_loads(pos_limits=(pos0, pos1), magnitude=magnitude):
+                        self.view.add_terminal_message(f"New load ({magnitude}) added from {pos0} to {pos1}")
                         self.update_display()
-                        print(3)
                         return True
-                print(4)
                 return False
-                
+
+        def save_button_clicked(self):
+                self.view.after_solve_gui()
+                self.update_display()
 
         # Starts the main event loop of the application
         def run(self):
